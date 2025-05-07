8f5c9fc3ca1b8d9581c82c131c07c02ff2605fbe
Lucas Georges Gabriel Charpentier Pacheco <lgcharpe@gmail.com>
2025-05-07 16:18:54 +0200

    [run_scripts] Adding revision name option for fast and fixing args

--- eval_zero_shot_fast.sh
+++ eval_zero_shot_fast.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 MODEL_PATH=$1
-BACKEND=$2
-BLIMP_DIR=${3:-"blimp/data"}
-EWOK_DIR=${4:-"ewok/data"}
-READING_DIR=${4:-"reading/data"}
+REVISION_NAME=${2:-"main"}
+BACKEND=$3
+BLIMP_DIR=${4:-"blimp/data"}
+EWOK_DIR=${5:-"ewok/data"}
+READING_DIR=${6:-"reading/data"}
 
-python -m evaluation_pipeline.sentence_zero_shot.run_zero_shot --model_path_or_name $MODEL_PATH --backend $BACKEND --task blimp --data_path "${BLIMP_DIR}/blimp_sampled" --save_predictions
-python -m evaluation_pipeline.sentence_zero_shot.run_zero_shot --model_path_or_name $MODEL_PATH --backend $BACKEND --task blimp --data_path "${BLIMP_DIR}/supplemental_sampled" --save_predictions
-python -m evaluation_pipeline.sentence_zero_shot.run_zero_shot --model_path_or_name $MODEL_PATH --backend $BACKEND --task ewok --data_path "${EWOK_DIR}/ewok_smapled" --save_predictions
-python -m evaluation_pipeline.reading.run --model_path_or_name $MODEL_PATH --backend $BACKEND --data_path "${READING_DIR}/all_measures.csv"
+python -m evaluation_pipeline.sentence_zero_shot.run_zero_shot --model_path_or_name $MODEL_PATH --backend $BACKEND --task blimp --data_path "${BLIMP_DIR}/blimp_sampled" --save_predictions --revivsion_name $REVISION_NAME
+python -m evaluation_pipeline.sentence_zero_shot.run_zero_shot --model_path_or_name $MODEL_PATH --backend $BACKEND --task blimp --data_path "${BLIMP_DIR}/supplemental_sampled" --save_predictions --revivsion_name $REVISION_NAME
+python -m evaluation_pipeline.sentence_zero_shot.run_zero_shot --model_path_or_name $MODEL_PATH --backend $BACKEND --task ewok --data_path "${EWOK_DIR}/ewok_sampled" --save_predictions --revivsion_name $REVISION_NAME
+python -m evaluation_pipeline.reading.run --model_path_or_name $MODEL_PATH --backend $BACKEND --data_path "${READING_DIR}/all_measures.csv" --revivsion_name $REVISION_NAME
