79e8ef099508af3e10dbbd17419934bdb965b984
Lucas Georges Gabriel Charpentier Pacheco <lgcharpe@gmail.com>
2025-05-07 16:22:12 +0200

    [run_scripts] Updating run command

--- eval_zero_shot_fast.sh
+++ eval_zero_shot_fast.sh
@@ -7,7 +7,7 @@ BLIMP_DIR=${4:-"blimp/data"}
 EWOK_DIR=${5:-"ewok/data"}
 READING_DIR=${6:-"reading/data"}
 
-python -m evaluation_pipeline.sentence_zero_shot.run_zero_shot --model_path_or_name $MODEL_PATH --backend $BACKEND --task blimp --data_path "${BLIMP_DIR}/blimp_sampled" --save_predictions --revivsion_name $REVISION_NAME
-python -m evaluation_pipeline.sentence_zero_shot.run_zero_shot --model_path_or_name $MODEL_PATH --backend $BACKEND --task blimp --data_path "${BLIMP_DIR}/supplemental_sampled" --save_predictions --revivsion_name $REVISION_NAME
-python -m evaluation_pipeline.sentence_zero_shot.run_zero_shot --model_path_or_name $MODEL_PATH --backend $BACKEND --task ewok --data_path "${EWOK_DIR}/ewok_sampled" --save_predictions --revivsion_name $REVISION_NAME
+python -m evaluation_pipeline.sentence_zero_shot.run --model_path_or_name $MODEL_PATH --backend $BACKEND --task blimp --data_path "${BLIMP_DIR}/blimp_sampled" --save_predictions --revivsion_name $REVISION_NAME
+python -m evaluation_pipeline.sentence_zero_shot.run --model_path_or_name $MODEL_PATH --backend $BACKEND --task blimp --data_path "${BLIMP_DIR}/supplemental_sampled" --save_predictions --revivsion_name $REVISION_NAME
+python -m evaluation_pipeline.sentence_zero_shot.run --model_path_or_name $MODEL_PATH --backend $BACKEND --task ewok --data_path "${EWOK_DIR}/ewok_sampled" --save_predictions --revivsion_name $REVISION_NAME
 python -m evaluation_pipeline.reading.run --model_path_or_name $MODEL_PATH --backend $BACKEND --data_path "${READING_DIR}/all_measures.csv" --revivsion_name $REVISION_NAME
