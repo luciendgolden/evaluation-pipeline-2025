805739fcfb6ee96061c19d301fead35d02a9be93
Lucas Georges Gabriel Charpentier Pacheco <lgcharpe@gmail.com>
2025-05-07 16:24:38 +0200

    [run_scripts] Updating reading data file names

--- eval_zero_shot_fast.sh
+++ eval_zero_shot_fast.sh
@@ -10,4 +10,4 @@ READING_DIR=${6:-"reading/data"}
 python -m evaluation_pipeline.sentence_zero_shot.run --model_path_or_name $MODEL_PATH --backend $BACKEND --task blimp --data_path "${BLIMP_DIR}/blimp_sampled" --save_predictions --revision_name $REVISION_NAME
 python -m evaluation_pipeline.sentence_zero_shot.run --model_path_or_name $MODEL_PATH --backend $BACKEND --task blimp --data_path "${BLIMP_DIR}/supplemental_sampled" --save_predictions --revision_name $REVISION_NAME
 python -m evaluation_pipeline.sentence_zero_shot.run --model_path_or_name $MODEL_PATH --backend $BACKEND --task ewok --data_path "${EWOK_DIR}/ewok_sampled" --save_predictions --revision_name $REVISION_NAME
-python -m evaluation_pipeline.reading.run --model_path_or_name $MODEL_PATH --backend $BACKEND --data_path "${READING_DIR}/all_measures.csv" --revision_name $REVISION_NAME
+python -m evaluation_pipeline.reading.run --model_path_or_name $MODEL_PATH --backend $BACKEND --data_path "${READING_DIR}/reading_data.csv" --revision_name $REVISION_NAME
